0584147cb836436752db86afd6822ff3322d7676
Victor Sérgio Silva Barros <victor.silva.barros@avanade.com>
2022-10-19 08:40:33 -0300

    update extract

---
+++ components/SideBar/SideBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from '@emotion/styled';
+import Profile from '../Profile';
+import Balance from '../Balance';
+import Menu from '../Menu';
+
+const SideBarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 360px;
+`
+
+export default function SideBar() {
+  return (
+    <SideBarContainer>
+        <Profile name="Victor" role="Software Engineer" agency="0001" accountNumber="123456789" />
+        <Balance amount={190000} />
+        <Menu />
+    </SideBarContainer>
+  )
+}
